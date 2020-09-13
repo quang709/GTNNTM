13f8c0d6f289e61ae3171ba3f0308d48e2019520
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-13 15:18:06 +0700

    Tìm giá trị nhỏ nhất trong mảng sử dụng phương thức

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GTNNTM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+            Console.WriteLine("array is : { 4, 12, 7, 8, 1, 6, 9 };");
+
+            int index = minValue(arr);
+         
+            Console.WriteLine("The smallest element in the array is: " + arr[index]);
+        }
+
+        private static int minValue(int[] arr)
+        {
+            int min = arr[0];
+            int index = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
